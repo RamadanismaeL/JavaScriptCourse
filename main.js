8708efa152a05ceddc94e7c7b752c5ec35899d77
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-13 23:31:10 +0200

    main updated

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ function welcomeMessage()
 {
     alert('Welcome to the WebSite')
 }
+
+var firstName = 'John'
+
+let age
+age = 25
+
+const address = 'Moz';
+
+// Boolean
+let status = true;
+
+// Array
+let fruits = [ 'apple', 'mango', 'orange', 2, true, [1,2,3] ]
+
+// Object
+let userDetails = { firstName: 'John', lastName: 'Doe', age: 62, isMarried: false }
