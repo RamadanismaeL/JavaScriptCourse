d712cd4e3e360d962ac792b0c7e4d6971ce9d337
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-13 23:47:03 +0200

    main updated

--- main.js
+++ main.js
@@ -22,9 +22,32 @@ let userDetails = { firstName: 'John', lastName: 'Doe', age: 62, isMarried: fals
 let userName = 'user1'
 userName = 'user2'
 
-// Addition Operator
+
 let num1 = 10
 let num2 = 5
 
+// Addition Operator (+)
 let sum = num1+num2
 console.log(sum)
+
+// Substraction Operator (-)
+console.log(num1 - num2)
+
+// Multiplication (*)
+console.log(num1*num2)
+
+// Division (/)
+console.log(num1/num2)
+
+// Remainder (%)
+console.log(num1%num2)
+
+// Exponentiation Operator (**)
+console.log(num1**num2)
+
+// equalsTo Operator (==) (return Boolean as result)
+let equals = num1 == num2
+console.log(equals)
+
+// notEqualsTo Operator (!=)
+console.log(num1 != num2)
