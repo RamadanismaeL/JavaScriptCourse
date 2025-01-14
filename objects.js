69bb72e76e019aa2262b6163659ea22517d47cf2
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-14 13:18:34 +0200

    updated

--- objects.js
+++ objects.js
@@ -106,3 +106,141 @@ console.log(typeof console.log)
 
 // Object + function = Method
 // JSON = JavaScript Object Notation
+
+let convert = JSON.stringify(product2)
+console.log(convert)
+console.log(typeof convert)
+let convert2 = JSON.parse(convert)
+console.log(convert2)
+
+localStorage.setItem('message', 'hello friend!')
+localStorage.setItem('score', JSON.stringify(score))
+console.log(localStorage.getItem('message'))
+console.log(localStorage.getItem('score'))
+console.log(JSON.parse(localStorage.getItem('score')))
+
+
+
+let computerMoveLocal = ''
+let resultLocal = ''
+
+let scoreLocal = JSON.parse(localStorage.getItem('scoreLocal')) || { wins: 0, losses: 0, ties: 0 }
+ console.log(scoreLocal)
+/*
+if(!scoreLocal)
+{
+    scoreLocal =
+    {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    }
+}
+*/
+function pickComputerMoveLocal()
+{
+    const randomNumberLocal = Math.random();
+    if(randomNumberLocal >= 0 && randomNumberLocal < 1/3) { computerMoveLocal = 'rock' }
+    else if(randomNumberLocal >= 1/3 && randomNumberLocal < 2/3) { computerMoveLocal = 'paper' }
+    else if(randomNumberLocal >= 2/3 && randomNumberLocal < 1) { computerMoveLocal = 'scissors' }
+
+    //return; // return undefined
+    console.log(computerMoveLocal)
+}
+
+function playGameLocal(playerMode)
+{
+    if(playerMode === 'rock')
+    {
+        pickComputerMoveLocal();
+
+        if(computerMoveLocal === 'rock') { resultLocal = 'Tie.' }
+        else if(computerMoveLocal === 'paper') { resultLocal = 'You lose.' }
+        else if(computerMoveLocal === 'scissors') { resultLocal = 'You win.' }
+    }
+    else if(playerMode === 'paper')
+    {
+        pickComputerMoveLocal()
+
+        if(computerMoveLocal === 'rock') { resultLocal = 'You win.' }
+        else if(computerMoveLocal === 'paper') { resultLocal = 'Tie.' }
+        else if(computerMoveLocal === 'scissors') { resultLocal = 'You lose.' }
+    }
+    else if(playerMode === 'scissors')
+    {
+        pickComputerMoveLocal()
+
+        if(computerMoveLocal === 'rock') { resultLocal = 'You lose.' }
+        else if(computerMoveLocal === 'paper') { resultLocal = 'You win.' }
+        else if(computerMoveLocal === 'scissors') { resultLocal = 'Tie.' }
+    }
+
+    if(resultLocal === 'You win.') { scoreLocal.wins++ }
+    else if(resultLocal === 'You lose.') { scoreLocal.losses++ }
+    else if(resultLocal === 'Tie.') { scoreLocal.ties++ }
+
+    localStorage.setItem('scoreLocal', JSON.stringify(scoreLocal))
+
+    alert(`You picked ${playerMode}. Computer picked ${computerMoveLocal}. ${resultLocal}\nWins: ${scoreLocal.wins}, Losses: ${scoreLocal.losses}, Ties: ${scoreLocal.ties}`)
+}
+
+function resetLocal()
+{
+    scoreLocal.wins = 0,
+    scoreLocal.losses = 0,
+    scoreLocal.ties = 0,
+    localStorage.removeItem('scoreLocal')
+
+    alert(`Cleared Successfuly\nWins: ${scoreLocal.wins}, Losses: ${scoreLocal.losses}, Ties: ${scoreLocal.ties}`)
+}
+
+function showResult()
+{
+    alert(`SHOW RESULTS\n${localStorage.getItem('scoreLocal')}`)
+}
+
+
+console.log('hello'.length)
+console.log('hello'.toUpperCase())
+
+const object1 = {
+    message: 'hello'
+}
+
+const object2 = object1;
+
+object1.message = 'Good job!'
+console.log(object1)
+console.log(object2)
+
+const object3 =
+{
+    message: 'Good job!'
+}
+
+console.log(object3 === object1)
+console.log(object1 === object2)
+
+const object4 =
+{
+    message1: 'Good job!',
+    price1: 799
+}
+//const message = object4.message
+const { message1, price1 } = object4
+console.log(message1)
+console.log(price1)
+
+const object5 = {
+    message1: message1
+}
+console.log(object5)
+
+const object6 =
+{
+    message1,
+    //method: function function6(){ console.log('method') }
+    method() { console.log('method') }
+}
+console.log(object6)
+object6.method()
