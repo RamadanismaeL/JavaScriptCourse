cfd672b425b3be23d26084507b1e1d1d72c78814
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-15 01:01:35 +0200

    in progress

--- scripts/arrays-and-loops.js
+++ scripts/arrays-and-loops.js
@@ -20,3 +20,24 @@ console.log(myArray)
 //myArray.splice(0) Remove all datas
 myArray.splice(0, 1)
 console.log(myArray)
+
+
+
+let i = 1
+while(i <= 5)
+{
+    console.log(i)
+    i++
+}
+
+for(let i = 1; i <= 5; i++) { console.log(i) }
+
+let randomNumber = 0
+
+while(randomNumber < 0.5)
+{
+    randomNumber = Math.random()
+}
+
+console.log(randomNumber)
+// 8:18:51
